0e7b575eee5c0e95285d939a42cd00a61505e5b1
Isaac F. Fonseca Segura <isaacfs74@gmail.com>
2025-09-22 03:47:44 -0600

    Update linked list destroy and create methods
    Same story that with the node, I was passing a copy of the pointer to
    the linked list instead of the actual pointer, so when destroying I
    could reach the address in the pointer, but then I left a dangling
    pointer due to pointing the copy to NULL, but not the actual pointer.
    
    Similar with create, as node's destroy changed now I need to pass a
    pointer to the node, not a copy of the pointer.

--- include/linked_list.h
+++ include/linked_list.h
@@ -6,13 +6,13 @@
 #include "node.h"
 
 typedef struct linked_list {
-  node* head;
-  node* tail;
+  node *head;
+  node *tail;
   int length;
-  void (*destroy)(struct linked_list* linked_list);
   void (*append)(struct linked_list* linked_list, node* node);
+  void (*destroy)(struct linked_list** linked_list);
 } linked_list;
 
-linked_list* create_linked_list(node* node);
+linked_list *create_linked_list(node** node);
 
 #endif // LINKED_LIST_H
--- src/linked_list.c
+++ src/linked_list.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "linked_list.h"
 
-static void destroy(linked_list* linked_list);
 static void append(linked_list* linked_list, node* node);
+static void destroy(linked_list **linked_list);
 
-linked_list* create_linked_list (node* node) {
-  linked_list* linked_list = (struct linked_list*) malloc(sizeof(struct linked_list)); 
+linked_list *create_linked_list(node **node_ptr) {
+  linked_list *linked_list =
+      (struct linked_list *)malloc(sizeof(struct linked_list));
+  struct node *node = *node_ptr;
   linked_list->head = node;
   linked_list->tail = node;
   linked_list->length = 1;
@@ -16,12 +17,18 @@ linked_list* create_linked_list (node* node) {
   return linked_list;
 };
 
-static void destroy(linked_list* linked_list) {
-  node* new_head = linked_list->head->next;
+static void destroy(linked_list **linked_list_ptr) {
+  struct linked_list *linked_list = *linked_list_ptr;
+  // Clear the nodes
   while (linked_list->head) {
-    linked_list->head->destroy(linked_list->head);
+    node *new_head = linked_list->head->next;
+    linked_list->head->destroy(&linked_list->head);
     linked_list->head = new_head;
   }
+  // Crear the linked list
+  linked_list->tail = NULL;
+  free(linked_list);
+  *linked_list_ptr = NULL;
   return;
 }
 
