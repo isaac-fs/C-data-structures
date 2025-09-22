1dda653004136773a13a1d99d534f49f6c9f76e3
Isaac F. Fonseca Segura <isaacfs74@gmail.com>
2025-09-22 04:25:44 -0600

    Update messages with "- " for leading sections

--- src/main.c
+++ src/main.c
@@ -4,25 +4,25 @@
 #include "node.h"
 
 void test_node() {
-  printf("Testing node implementation:\n");
+  printf("- Testing node implementation:\n");
   node *node = create_node("hello");
-  printf("Created new node with value %s\n", node->value);
+  printf("- Created new node with value %s\n", node->value);
   printf("Node next: %p\n", node->next);
-  printf("Destroying node...\n");
+  printf("- Destroying node...\n");
   node->destroy(&node);
   printf("Node: %p\n", node);
   printf("\n");
 }
 
 void test_linked_list() {
-  printf("Testing linked list implementation:\n");
+  printf("- Testing linked list implementation:\n");
   node *node = create_node("hello");
   linked_list *linked_list = create_linked_list(&node);
-  printf("Created new linked list\n");
+  printf("- Created new linked list\n");
   printf("Linked list head: %s\n", linked_list->head->value);
   printf("Linked list tail: %s\n", linked_list->tail->value);
   printf("Linked list length: %d\n", linked_list->length);
-  printf("Appeding node to list...\n");
+  printf("- Appeding node to list...\n");
   struct node *node2 = create_node("world");
   linked_list->append(&linked_list, &node2);
   printf("Linked list head: %s\n", linked_list->head->value);
@@ -32,7 +32,7 @@ void test_linked_list() {
   linked_list->print(linked_list);
   printf("- Destroying linked list...\n");
   linked_list->destroy(&linked_list);
-  printf("linked list: %p\n", linked_list);
+  printf("Linked list: %p\n", linked_list);
   printf("\n");
 }
 
