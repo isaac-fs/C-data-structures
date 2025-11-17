ac07b46110ca5bceeba0f8443c70b778408cf2d7
Isaac F. Fonseca Segura <isaacfs74@gmail.com>
2025-11-17 05:06:40 -0600

    Hash table initial implementation done

--- include/hash_table.h
+++ include/hash_table.h
@@ -2,14 +2,20 @@
 #define HASH_TABLE_H
 
 #include <stdbool.h>
+#include <stdlib.h>
 
-typedef struct {
-  char* value;
-  int key;
+#define TABLE_SIZE 1000
+
+typedef unsigned int ht_key;
+
+typedef struct hash_table {
+  char **values;
+  ht_key (*insert)(struct hash_table **hash_table, char *value);
+  char *(*extract)(struct hash_table **hash_table, ht_key key);
+  void (*destroy)(struct hash_table **hash_table_ptr);
+  size_t table_size;
 } hash_table;
 
-int hash_function(char* value);
-bool insert(char* input);
-char* extract(int key);
+hash_table *create_hash_table(void);
 
 #endif // HASH_TABLE_H
--- src/hash_table.c
+++ src/hash_table.c
@@ -1,16 +1,65 @@
 #include <stdio.h>
 
 #include "hash_table.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int hash_function(char* value) {
-  return 0;
+static unsigned int hash(const char *value, size_t value_len);
+ht_key insert(hash_table **hash_table_ptr, char *input);
+char *extract(hash_table **hash_table_ptr, ht_key key);
+void destroy(hash_table **hash_table_ptr);
+
+hash_table *create_hash_table(void) {
+  hash_table *hash_table =
+      (struct hash_table *)malloc(sizeof(struct hash_table));
+  hash_table->table_size = TABLE_SIZE;
+  hash_table->values = (char **)malloc(sizeof(char *) * hash_table->table_size);
+  for (size_t i = 0; i < hash_table->table_size; i++) {
+    hash_table->values[i] = NULL;
+  }
+  hash_table->insert = insert;
+  hash_table->extract = extract;
+  hash_table->destroy = destroy;
+  return hash_table;
+}
+
+static ht_key hash(const char *value, size_t value_len) {
+  unsigned int sum = 0;
+
+  for (size_t i = 0; i < value_len; i++) {
+    sum += (int)value[i];
+  }
+
+  return sum % TABLE_SIZE;
 };
 
-bool insert(char* input) {
-  return true;
+ht_key insert(hash_table **hash_table_ptr, char *value) {
+  hash_table *hash_table = *hash_table_ptr;
+  size_t size = strlen(value);
+  ht_key key = hash(value, size);
+  hash_table->values[key] = (char *)malloc(size * sizeof(char));
+  memcpy(hash_table->values[key], value, size);
+  return key;
 };
 
-char* extract(int key) {
-  return NULL;
+char *extract(hash_table **hash_table_ptr, ht_key key) {
+  hash_table *hash_table = *hash_table_ptr;
+  return hash_table->values[key];
 };
 
+void destroy(hash_table **hash_table_ptr) {
+  hash_table *hash_table = *hash_table_ptr;
+  for (size_t i = 0; i < hash_table->table_size; i++) {
+    if (hash_table->values[i]) {
+      free(hash_table->values[i]);
+      hash_table->values[i] = NULL;
+    }
+  }
+
+  free(hash_table->values);
+  free(hash_table);
+  *hash_table_ptr = NULL;
+
+  return;
+}
